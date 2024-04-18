4149f3cc0f889fff90c19f3745c928a34e5e0fe5
cyberpwn <danielmillst@gmail.com>
2024-04-17 20:04:13 -0400

    fix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.1
+
+* Remove home test in main file
+
 ## 1.0.0
 
 * Initial release
--- lib/tabby.dart
+++ lib/tabby.dart
@@ -1,6 +1,5 @@
 library tabby;
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class TabbyTab {
@@ -112,51 +111,4 @@ class TabbyState extends State<Tabby> {
                 items: tabs.map((e) => e.bottomNavigationBarItem).toList(),
               ),
             );
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  late int _selectedIndex;
-
-  @override
-  void initState() {
-    _selectedIndex = 0; // or load from storage
-    super.initState();
-  }
-
-  void _changeTab(int index) => setState(() {
-        _selectedIndex = index;
-        // or save to storage
-      });
-
-  @override
-  Widget build(BuildContext context) => Tabby(
-          initialIndex: _selectedIndex,
-          onIndexChanged: _changeTab,
-          // Screen widths above this threshold use navigation rail instead of bottom navigation bar
-          widthThreshold: 600,
-          showLabels: true,
-          tabs: [
-            TabbyTab(
-                icon: Icons.home_outlined,
-                selectedIcon: Icons.home_rounded,
-                label: "Home",
-                child: Text("Home Tab Content")),
-            TabbyTab(
-                icon: Icons.ac_unit_outlined,
-                selectedIcon: Icons.ac_unit_rounded,
-                builder: (context) => Text("Builder tab content"),
-                label: "Builder"),
-            TabbyTab(
-                // Only show this tab if in debug mode
-                shouldShow: () => kDebugMode,
-                icon: Icons.access_time_filled,
-                label: "Builder")
-          ]);
 }
